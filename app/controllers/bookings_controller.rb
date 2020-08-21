7cd78a94ea9d8a0be0057e4bf07667fc504eba34
Andre Ferrer <andrerferrer@hotmail.com>
2020-08-21 09:55:43 -0300

    Add bookings index and create (to be finished)

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,9 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def create
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: 'restaurants#index'
   resources :restaurants, only: %i[index show new create destroy] do
     resources :reviews, only: :create
+    resources :bookings, only: :create
   end
+  
+  resources :bookings, only: :index
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
