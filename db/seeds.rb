6b29356ebf8736f945bd6422f0b4bb9f5fe599fa
Andre Ferrer <andrerferrer@hotmail.com>
2020-08-21 10:14:06 -0300

    add refs to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,8 @@ address = {
   { name: "Verdeen", **address }
 ].each(&Restaurant.method(:create!))
 
+# To help with seeding date and datetimes
+# https://stackoverflow.com/questions/5474164/rails-seeding-database-data-and-date-formats
 Booking.create! booked_at: DateTime.tomorrow.noon, restaurant: Restaurant.last
 
 puts "Let's drink a beer"
