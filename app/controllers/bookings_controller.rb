8127bb30c8bac314e0963861c7ba09a7cfd8f8c8
Andre Ferrer <andrerferrer@hotmail.com>
2020-08-21 10:17:10 -0300

    implement bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,6 +4,14 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(strong_params)
+    @booking.save ? redirect_to(bookings_path) : render(:index)
+  end
+
+  private
+
+  def strong_params
+    params.require(:booking).permit(:booked_at, :restaurant_id)
   end
 
 end
